de3c6f16384557830918583318a31fd178a40922
David Felix <davidffelix@hotmail.com>
2022-07-22 18:43:37 -0300

    Create new component

---
+++ lib/questao.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp()); //Componente raíz
+
+class _PerguntaAppState extends State<PerguntaApp> { // Conexão entre estado e o Stateful e controle do estado do componente Stateful.
+  var _perguntaSelecionada = 0;
+
+  void _responder() {
+    setState((){
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é sua cor favorita?',
+      'Qual é seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[_perguntaSelecionada]),
+            TextButton(
+              child: Text('Resposta 1'),
+              onPressed: _responder,
+            ),
+            TextButton(
+              child: Text('Resposta 2'),
+              onPressed: _responder,
+            ),
+            TextButton(
+              child: Text('Resposta 3'),
+              onPressed: _responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
