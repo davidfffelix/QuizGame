d13dc7d4ed98e79bcb9e1ee342d478116d69f97a
David Felix <davidffelix@hotmail.com>
2022-07-28 22:28:11 -0300

    Update code in questao

--- lib/questao.dart
+++ lib/questao.dart
@@ -10,12 +10,13 @@ class Questao extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: EdgeInsets.all(10),
       child: Text(
-          texto,
-          style: TextStyle(
-            fontSize: 28,
-          ),
-          textAlign: TextAlign.center,
+        texto,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
