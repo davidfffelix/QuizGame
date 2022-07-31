35c1ddd632e8a7fe98622caf76c7c5222d4f1d32
David Felix <davidffelix@hotmail.com>
2022-07-31 10:31:49 -0300

    Customize button

--- lib/resposta.dart
+++ lib/resposta.dart
@@ -8,11 +8,22 @@ class Resposta extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: Text(
-          texto
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          // minimumSize: Size(100, 40),
+          textStyle: TextStyle(fontSize: 15),
+          primary: Colors.indigo, // Cor de fundo
+          onPrimary: Colors.white, // Cor da fonte
+        ),
+        child: Text(
+            texto
+        ),
+        onPressed: () {},
       ),
-      onPressed: null,
     );
   }
 }
+
+
