c9382922629cbea29c296802791718b5653b61a0
David Felix <davidffelix@hotmail.com>
2022-08-03 21:49:49 -0300

    Update list in main

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,11 @@ class _PerguntaAppState extends State<PerguntaApp> { // Conexão entre estado e 
 
   @override
   Widget build(BuildContext context) {
-    final List<String> perguntas = [
-      'Qual é sua cor favorita?',
-      'Qual é seu animal favorito?',
+    final List<Map<String, Object>> perguntas = [
+        {'texto': 'Qual é sua cor favorita?'
+      },
+        {'texto': 'Qual é seu animal favorito?'
+      },
     ];
 
     return MaterialApp(
